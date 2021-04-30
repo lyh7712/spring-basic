229dfb479c160dfc5f56a9fc8005d361b96aefec
lyh7712 <mmj2rv9@kakao.com>
2021-04-30 19:25:39 +0900

    refactor: AppConfig 수정(어노테이션 기반 스프링 컨테이너 생성)

--- src/main/java/lyh7712/springbasic/AppConfig.java
+++ src/main/java/lyh7712/springbasic/AppConfig.java
@@ -2,6 +2,7 @@ package lyh7712.springbasic;
 
 
 import lyh7712.springbasic.discount.DiscountPolicy;
+import lyh7712.springbasic.discount.FixDiscountPolicy;
 import lyh7712.springbasic.discount.RateDiscountPolicy;
 import lyh7712.springbasic.member.MemberMemoryRepository;
 import lyh7712.springbasic.member.MemberRepository;
@@ -9,24 +10,31 @@ import lyh7712.springbasic.member.MemberService;
 import lyh7712.springbasic.member.MemberServiceImpl;
 import lyh7712.springbasic.order.OrderService;
 import lyh7712.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 객체 생성과 동시에 구현체 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberMemoryRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
+        return new FixDiscountPolicy();
     }
 
 }
