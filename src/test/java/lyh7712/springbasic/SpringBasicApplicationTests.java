bfbe3b3e1e40c6681d431a6bb21d66ce51c47b01
lyh7712 <mmj2rv9@kakao.com>
2021-08-06 16:40:43 +0900

    test

--- src/test/java/lyh7712/springbasic/SpringBasicApplicationTests.java
+++ src/test/java/lyh7712/springbasic/SpringBasicApplicationTests.java
@@ -1,11 +1,9 @@
 package lyh7712.springbasic;
 
-import lyh7712.springbasic.user.config.SwaggerWebMvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(SwaggerWebMvc.class)
 @SpringBootTest
 class SpringBasicApplicationTests {
 
